aade0a1771874d6ff4677251d5df2b8f54e85ce7
John Palacios <105229330+Matheu5x@users.noreply.github.com>
2025-08-28 23:14:17 -0500

    Add files via upload

--- run_server.py
+++ run_server.py
@@ -1,4 +1,19 @@
+# from app import app
+#
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000, debug=False)
+# run_server.py
+# import os, sys
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# if BASE_DIR not in sys.path:
+#     sys.path.insert(0, BASE_DIR)
+
+import os, sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=False)
+
